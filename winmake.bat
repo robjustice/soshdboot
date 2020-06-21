c986c4fba58a87bf8a05653d319d29711640fec6
robjustice <j.robert.justice@gmail.com>
2020-06-21 16:25:39 +1000

    update winmake

--- winmake.bat
+++ winmake.bat
@@ -63,7 +63,7 @@ if "%1" equ "clean" (
 :clean
 echo y|1>nul 2>nul rd lst /s
 echo y|1>nul 2>nul rd obj /s
-echo y|1>nul 2>nul rd bin /s
+echo y|1>nul 2>nul rd out /s
 goto :EOF
 )
 
@@ -79,7 +79,7 @@ goto :EOF
 2>nul md obj\rom
 2>nul md obj\disk3
 2>nul md obj\boot
-2>nul md bin
+2>nul md out
 goto :EOF
 
 rem Assemble SOS and add SOS.KERNEL to the disk images
@@ -101,13 +101,13 @@ rem Assemble SOS and add SOS.KERNEL to the disk images
 %CA65% src/sos/bufmgr.s -l lst/sos/bufmgr.lst -o obj/sos/bufmgr.o
 %CA65% src/sos/memmgr.s -l lst/sos/memmgr.lst -o obj/sos/memmgr.o
 %CA65% src/sos/padding.s -l lst/sos/padding.lst -o obj/sos/padding.o
-%LD65% obj/sos/sosldr.o obj/sos/init.o obj/sos/sysglob.o obj/sos/bfm.init2.o obj/sos/bfm.o obj/sos/oprmsg.o obj/sos/ipl.o obj/sos/umgr.o obj/sos/Problock3Kernel.o obj/sos/syserr.o obj/sos/devmgr.o obj/sos/scmgr.o obj/sos/fmgr.o obj/sos/cfmgr.o obj/sos/bufmgr.o obj/sos/memmgr.o obj/sos/padding.o -o bin/SOS.KERNEL#0C0000 -C build/apple3.cfg
+%LD65% obj/sos/sosldr.o obj/sos/init.o obj/sos/sysglob.o obj/sos/bfm.init2.o obj/sos/bfm.o obj/sos/oprmsg.o obj/sos/ipl.o obj/sos/umgr.o obj/sos/Problock3Kernel.o obj/sos/syserr.o obj/sos/devmgr.o obj/sos/scmgr.o obj/sos/fmgr.o obj/sos/cfmgr.o obj/sos/bufmgr.o obj/sos/memmgr.o obj/sos/padding.o -o out/SOS.KERNEL#0C0000 -C build/apple3.cfg
 java -jar %AC% -d %DISK% SOS.KERNEL
-java -jar %AC% -p %DISK% SOS.KERNEL SOS $0000 < bin/SOS.KERNEL#0C0000
+java -jar %AC% -p %DISK% SOS.KERNEL SOS $0000 < out/SOS.KERNEL#0C0000
 java -jar %AC% -d %TDMDISK% SOS.KERNEL
-java -jar %AC% -p %TDMDISK% SOS.KERNEL SOS $0000 < bin/SOS.KERNEL#0C0000
+java -jar %AC% -p %TDMDISK% SOS.KERNEL SOS $0000 < out/SOS.KERNEL#0C0000
 java -jar %AC% -d %PLDISK% SOS.KERNEL
-java -jar %AC% -p %PLDISK% SOS.KERNEL SOS $0000 < bin/SOS.KERNEL#0C0000
+java -jar %AC% -p %PLDISK% SOS.KERNEL SOS $0000 < out/SOS.KERNEL#0C0000
 rem echo off
 goto :EOF
 
@@ -116,25 +116,25 @@ rem Assemble Monitor ROM
 %CA65% src/rom/diskio.s -l lst/rom/diskio.lst -o obj/rom/diskio.o
 %CA65% src/rom/saratests.s -l lst/rom/saratests.lst -o obj/rom/saratests.o
 %CA65% src/rom/monitor.s -l lst/rom/monitor.lst -o obj/rom/monitor.o
-%LD65% obj/rom/diskio.o obj/rom/saratests.o obj/rom/monitor.o -o bin/apple3.rom -C build/apple3.cfg
+%LD65% obj/rom/diskio.o obj/rom/saratests.o obj/rom/monitor.o -o out/apple3.rom -C build/apple3.cfg
 goto :EOF
 
 rem Assemble disk3 driver and update SOS.DRIVER file in disk images
 :disk3
 %CA65% src/disk3/disk3.s -l lst/disk3/disk3.lst -o obj/disk3/disk3.o
 %LD65% obj/disk3/disk3.o -o obj/disk3/disk3.o65 -C build/Apple3_o65.cfg
-java -jar %AC% -g %DISK% SOS.DRIVER > bin/SOS.DRIVER#0c0000
-1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 bin/SOS.DRIVER#0c0000
+java -jar %AC% -g %DISK% SOS.DRIVER > out/SOS.DRIVER#0c0000
+1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 out/SOS.DRIVER#0c0000
 java -jar %AC% -d %DISK% SOS.DRIVER
-java -jar %AC% -p %DISK% SOS.DRIVER SOS $0000 < bin/SOS.DRIVER#0c0000
-java -jar %AC% -g %TDMDISK% SOS.DRIVER > bin/SOS.DRIVER_TDM#0c0000
-1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 bin/SOS.DRIVER_TDM#0C0000
+java -jar %AC% -p %DISK% SOS.DRIVER SOS $0000 < out/SOS.DRIVER#0c0000
+java -jar %AC% -g %TDMDISK% SOS.DRIVER > out/SOS.DRIVER_TDM#0c0000
+1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 out/SOS.DRIVER_TDM#0C0000
 java -jar %AC% -d %TDMDISK% SOS.DRIVER
-java -jar %AC% -p %TDMDISK% SOS.DRIVER SOS $0000 < bin/SOS.DRIVER_TDM#0C0000
-java -jar %AC% -g %PLDISK% SOS.DRIVER > bin/SOS.DRIVER_PL#0c0000
-1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 bin/SOS.DRIVER_PL#0C0000
+java -jar %AC% -p %TDMDISK% SOS.DRIVER SOS $0000 < out/SOS.DRIVER_TDM#0C0000
+java -jar %AC% -g %PLDISK% SOS.DRIVER > out/SOS.DRIVER_PL#0c0000
+1>nul %PYTHON% %A3DUTIL% update obj/disk3/disk3.o65 out/SOS.DRIVER_PL#0C0000
 java -jar %AC% -d %PLDISK% SOS.DRIVER
-java -jar %AC% -p %PLDISK% SOS.DRIVER SOS $0000 < bin/SOS.DRIVER_PL#0C0000
+java -jar %AC% -p %PLDISK% SOS.DRIVER SOS $0000 < out/SOS.DRIVER_PL#0C0000
 goto :EOF
 
 PLASMADISK
@@ -142,17 +142,17 @@ PLASMADISK
 rem Assemble Boot Loaders
 :boot
 %CA65% src/boot/bootloader_sos.s -l lst/boot/bootloader_sos.lst -o obj/boot/bootloader_sos.o
-%LD65% obj/boot/bootloader_sos.o -o bin/bootloader_1blk.bin -C build/apple3.cfg
+%LD65% obj/boot/bootloader_sos.o -o out/bootloader_1blk.bin -C build/apple3.cfg
 %CA65% src/boot/bootloader_sos_tdm.s -l lst/boot/bootloader_sos_tdm.lst -o obj/boot/bootloader_sos_tdm.o
-%LD65% obj/boot/bootloader_sos_tdm.o -o bin/bootloader_tdm_1blk.bin -C build/apple3.cfg
+%LD65% obj/boot/bootloader_sos_tdm.o -o out/bootloader_tdm_1blk.bin -C build/apple3.cfg
 %CA65% src/boot/bootloader_prodos_sos.s -l lst/boot/bootloader_prodos_sos.lst -o obj/boot/bootloader_prodos_sos.o
-%LD65% obj/boot/bootloader_prodos_sos.o -o bin/bootloader_2blk.bin -C build/apple3bs.cfg
+%LD65% obj/boot/bootloader_prodos_sos.o -o out/bootloader_2blk.bin -C build/apple3bs.cfg
 %CA65% src/boot/bootloader_prodos_sos_tdm.s -l lst/boot/bootloader_prodos_sos_tdm.lst -o obj/boot/bootloader_prodos_sos_tdm.o
-%LD65% obj/boot/bootloader_prodos_sos_tdm.o -o bin/bootloader_tdm_2blk.bin -C build/apple3bs.cfg
-1>nul %PYTHON% %BOOTLOADER% bin\bootloader_1blk.bin %BOOTDISK%
-1>nul %PYTHON% %BOOTLOADER% bin\bootloader_tdm_1blk.bin %BOOTTDMDISK%
-1>nul %PYTHON% %BOOTLOADER% bin\bootloader_2blk.bin %DISK%
-1>nul %PYTHON% %BOOTLOADER% bin\bootloader_tdm_2blk.bin %TDMDISK%
-1>nul %PYTHON% %BOOTLOADER% bin\bootloader_2blk.bin %PLDISK%
+%LD65% obj/boot/bootloader_prodos_sos_tdm.o -o out/bootloader_tdm_2blk.bin -C build/apple3bs.cfg
+1>nul %PYTHON% %BOOTLOADER% out\bootloader_1blk.bin %BOOTDISK%
+1>nul %PYTHON% %BOOTLOADER% out\bootloader_tdm_1blk.bin %BOOTTDMDISK%
+1>nul %PYTHON% %BOOTLOADER% out\bootloader_2blk.bin %DISK%
+1>nul %PYTHON% %BOOTLOADER% out\bootloader_tdm_2blk.bin %TDMDISK%
+1>nul %PYTHON% %BOOTLOADER% out\bootloader_2blk.bin %PLDISK%
 goto :EOF
 
