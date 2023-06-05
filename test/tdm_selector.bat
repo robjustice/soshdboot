b7bb676363ceb80a4255117661745c81afd109ae
Robert Justice <j.robert.justice@gmail.com>
2023-06-05 14:11:53 +1000

    add Softcard3 launcher to disk images

--- test/tdm_selector.bat
+++ test/tdm_selector.bat
@@ -4,7 +4,7 @@ rem adjust to your mame path
 set MAMEPATH=C:\Storage\_emu\mame
 
 rem copy in the new apple3 rom, assumes the apple3 folder to be there
-copy /b ..\rom\apple3hdboot.rom %MAMEPATH%\roms\apple3\
+copy /b ..\rom\apple3hdboot.rom %MAMEPATH%\roms\apple3\soshdboot.bin
 
 rem run mame ( note, -nothrottle in command for speedy testing)
-%MAMEPATH%\mame64 apple3 -rompath %MAMEPATH%\roms -skip_gameinfo -resolution 640x480 -window -nothrottle -sl1 cffa2 -hard1 ..\disks\sos_selector_tdm_hd.po
+%MAMEPATH%\mame apple3 -rompath %MAMEPATH%\roms -bios 1 -skip_gameinfo -resolution 640x480 -window -nothrottle -sl1 cffa2 -hard1 ..\disks\sos_selector_tdm_hd.po -sl2 softcard3
